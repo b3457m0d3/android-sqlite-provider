8015a04cffb24442f385d2fa92f4674bb3ac1ae2
samkirton <samkirton@me.com>
2013-07-09 13:48:08 +1000

    ;Removed the Class<?> c argument from the insert method in SQLProvider, the class can be retrieved from the baseSQLModel instance; Added an insertArray() method to SQLProvider which takes an array of models; Added a count() method to SQLProvider which queries the table with the SQL count(*) function; removed an erroneous import; Updated the README example to reflect the new insert statement arguments

--- src/com/memtrip/application/sql/SQLProvider.java
+++ src/com/memtrip/application/sql/SQLProvider.java
@@ -1,9 +1,6 @@
 package com.memtrip.application.sql;
 
-
 import java.io.Closeable;
-
-import com.memtrip.application.utils.ReflectionHelper;
 
 import android.content.Context;
 import android.database.Cursor;
@@ -43,20 +40,37 @@ public class SQLProvider implements Closeable {
 	}
 	
 	/**
-	 * Insert a baseSQLModel into the table that relates to the provided class interface
-	 * @param	c	The baseSQLModel class interface that relates to a SQLite table
+	 * Insert a baseSQLModel into the table that relates to the baseSQLModel type
 	 * @param	baseSQLModel	The object being inserted into the SQLite table
 	 * @return	The unique id of the created row
 	 */
-	public long insert(Class<?> c, BaseSQLModel baseSQLModel) {
-		String tableName = ReflectionHelper.getStaticStringField(c, FIELD_TABLE_NAME);
+	public long insert(BaseSQLModel baseSQLModel) {
+		String tableName = ReflectionHelper.getStaticStringField(baseSQLModel.getClass(), FIELD_TABLE_NAME);
 		return database.insertOrThrow(tableName, baseSQLModel.getNullColumnHack(), baseSQLModel.toContentValues());
+	}
+	
+	/**
+	 * Insert an array of baseSQLModel into the table that relates to the array database
+	 * @param	baseSQLModelArray	The array of objects being inserted into the SQLite table
+	 * @return	The unique ids of the created rows
+	 */
+	public long[] insertArray(BaseSQLModel[] baseSQLModelArray) {
+		if (baseSQLModelArray != null && baseSQLModelArray.length > 0) {
+			long[] respnseIds = new long[baseSQLModelArray.length];
+			
+			for (int i = 0; i < baseSQLModelArray.length; i++) {
+				respnseIds[i] = this.insert(baseSQLModelArray[i]);
+			}
+			
+			return respnseIds;
+		} else {
+			return null;
+		}
 	}
 	
 	/**
 	 * Selects all baseSQLModel elements from the provided class, it requires a baseSQLModel
 	 * instance to retrieve the columns that should be populated 
-	 * TODO: This method should return a genric that can be cast to an BaseSQLModel class
 	 * @param	c	The baseSQLModel class that is being selected from the database
 	 * @param	baseSQLModel	An instance of the baseSQLModel class that is being selected from the database
 	 * @param	order	The order results should be returned (ASC or DESC)
@@ -107,6 +121,24 @@ public class SQLProvider implements Closeable {
 		
 		T[] result = SQLDatabaseHelper.retreiveSQLSelectResults(c, baseSQLModel, cursor);
 		return (T[])result;
+	}
+	
+	/**
+	 * Count the number of rows in the provided table
+	 * @param	c	The baseSQLModel class that should have its rows counted
+	 * @return	The number of rows in the table
+	 */
+	public <T> int count(Class<T> c) {
+		String tableName = ReflectionHelper.getStaticStringField(c, FIELD_TABLE_NAME);
+		Cursor cursor = database.rawQuery("select count(*) from " + tableName, null);
+		
+		// ensure there is at least one row and one column
+		cursor.moveToFirst();
+		if (cursor.getCount() > 0 && cursor.getColumnCount() > 0) {
+			return cursor.getInt(0);
+		} else {
+			return 0;
+		}
 	}
 	
 	/**
